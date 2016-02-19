d8cdd2f35837171b1ff08cdf98df1393ec963d29
Justin Blake <justin@hentzia.com>
2016-02-19 14:26:23 -0500

    Add unit tests to helloworld example
    Closes #5

---
+++ examples/helloworld/tests/unit_tests.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from tdubs import calling, Stub
+
+from examples.helloworld.app import say_hello, say_goodbye, GreetingController
+
+
+class TestSayHello(TestCase):
+    def test_says_hello_to_name(self):
+        result = say_hello('Justin')
+        self.assertEqual(result, 'Hello Justin!\n')
+
+
+class TestSayGoodbye(TestCase):
+    def test_says_goodbye_to_name(self):
+        result = say_goodbye('Mary')
+        self.assertEqual(result, 'Goodbye Mary!\n')
+
+
+class TestGreetingController(TestCase):
+    def setUp(self):
+        greeter = Stub('greeter')
+        calling(greeter).passing('the name').returns('the greeting')
+
+        self.controller = GreetingController(greeter)
+        self.request = Stub('request', kwargs={'name': 'the name'})
+        self.response = Stub('response')
+
+    def test_sets_greeter_result_as_response_text(self):
+        response = self.controller(self.request, self.response)
+        self.assertEqual(response.text, 'the greeting')
