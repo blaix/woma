3628fae9100fb96bf6a70271da1cf2c06a4abb2c
Justin Blake <justin@hentzia.com>
2016-04-18 05:24:01 -0400

    Replace unittest.mock with tdubs
    Closes #17

--- tests/router_tests.py
+++ tests/router_tests.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from unittest.mock import Mock
+
+from tdubs import calling, verify, Mock, Stub
 
 from woma.router import Router, Route
 from woma.endpoints import not_found
@@ -7,7 +8,7 @@ from woma.endpoints import not_found
 
 class RouterTestCase(TestCase):
     def setUp(self):
-        self.routes = Mock()
+        self.routes = Mock('routes')
         self.router = Router(self.routes)
 
 
@@ -16,32 +17,29 @@ class TestRouterInit(RouterTestCase):
 
     def test_sets_default_route_to_not_found_handler(self):
         default = Route(path=None, endpoint=not_found)
-        self.routes.setdefault.assert_called_once_with(default)
+        verify(self.routes.setdefault).called_with(default)
 
 
 class TestRouterAdd(RouterTestCase):
     """router.add(path, endpoint)"""
 
     def test_adds_route_to_routes(self):
-        path = '/path'
-        endpoint = Mock()
-        expected_route = Route(path=path, endpoint=endpoint)
-
-        self.router.add(path, endpoint)
-
-        self.routes.add.assert_called_once_with(expected_route)
+        endpoint = Stub('endpoint')
+        expected_route = Route(path='/path', endpoint=endpoint)
+        self.router.add('/path', endpoint)
+        verify(self.routes.add).called_with(expected_route)
 
 
 class TestRouterSetDefault(RouterTestCase):
     """router.setdefault(endpoint)"""
 
     def test_sets_default_route_to_endpoint(self):
-        endpoint = Mock()
+        endpoint = Stub('endpoint')
         expected_route = Route(path=None, endpoint=endpoint)
 
         self.router.setdefault(endpoint)
 
-        self.routes.setdefault.assert_called_with(expected_route)
+        verify(self.routes.setdefault).called_with(expected_route)
 
 
 class TestRouterCall(RouterTestCase):
@@ -49,19 +47,20 @@ class TestRouterCall(RouterTestCase):
 
     def setUp(self):
         super(TestRouterCall, self).setUp()
+
         self.environ = {'PATH_INFO': '/path'}
-        self.start_response = Mock()
-        self.response = self.router(self.environ, self.start_response)
+        start_response = Stub('start_response')
 
-    def test_gets_route_for_request_path(self):
-        self.routes.get.assert_called_once_with('/path')
+        route = Stub('route', kwargs={'expected': 'kwargs'})
+        calling(route.endpoint).passing(self.environ, start_response).returns(
+            'endpoint response')
 
-    def test_adds_router_kwargs_to_environ(self):
-        route = self.routes.get.return_value
-        self.assertEqual(self.environ['router.kwargs'], route.kwargs)
+        calling(self.routes.get).passing('/path').returns(route)
+
+        self.response = self.router(self.environ, start_response)
+
+    def test_adds_route_kwargs_to_environ(self):
+        self.assertEqual(self.environ['router.kwargs'], {'expected': 'kwargs'})
 
     def test_returns_call_to_routes_endpoint(self):
-        route = self.routes.get.return_value
-        route.endpoint.assert_called_once_with(
-            self.environ, self.start_response)
-        self.assertEqual(self.response, route.endpoint.return_value)
+        self.assertEqual(self.response, 'endpoint response')
